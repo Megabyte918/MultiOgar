d2c8ac6c2f1167bf48a323b5588fc875edebf9fa
Tyler3D <Tylermanrique@gmail.com>
2016-11-18 23:01:15 -0500

    Update index.js

--- src/gamemodes/index.js
+++ src/gamemodes/index.js
@@ -26,6 +26,9 @@ var get = function (id) {
         case 5: // Rainbow
             mode = new module.exports.Rainbow();
             break;
+         case 6: // LMS
+         	mode = new module.exports.LMS();
+         	break;
         default: // FFA is default
             mode = new module.exports.FFA();
             break;

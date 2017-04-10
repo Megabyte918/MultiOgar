445ad257c3568b5c9aa20c065907411fed47319a
Agarian <Agarian@users.noreply.github.com>
2017-04-09 20:27:41 -0400

    "Beautify" Virus

--- src/entity/Virus.js
+++ src/entity/Virus.js
@@ -5,43 +5,39 @@ function Virus() {
     this.cellType = 2;
     this.isSpiked = true;
     this.isMotherCell = false; // Not to confuse bots
-    this.setColor({ r: 0x33, g: 0xff, b: 0x33 });
+    this.setColor({
+        r: 0x33,
+        g: 0xff,
+        b: 0x33
+    });
 }
-
 module.exports = Virus;
 Virus.prototype = new Cell();
-
 // Main Functions
-
-Virus.prototype.canEat = function (cell) {
+Virus.prototype.canEat = function(cell) {
     // cannot eat if virusMaxAmount is reached
-    if (this.gameServer.nodesVirus.length < this.gameServer.config.virusMaxAmount)
-        return cell.cellType == 3; // virus can eat ejected mass only
+    if (this.gameServer.nodesVirus.length < this.gameServer.config.virusMaxAmount) return cell.cellType == 3; // virus can eat ejected mass only
 };
-
-Virus.prototype.onEat = function (prey) {
+Virus.prototype.onEat = function(prey) {
     // Called to eat prey cell
     this.setSize(Math.sqrt(this._sizeSquared + prey._sizeSquared));
-    
     if (this._size >= this.gameServer.config.virusMaxSize) {
         this.setSize(this.gameServer.config.virusMinSize); // Reset mass
         this.gameServer.shootVirus(this, prey.boostDirection.angle());
     }
 };
-
-Virus.prototype.onEaten = function (c) {
+Virus.prototype.onEaten = function(c) {
     if (!c.owner) return; // Only players can explode
-    var minSize = this.gameServer.config.playerMinSize,                         // maximum size of small splits
-    cellsLeft = this.gameServer.config.playerMaxCells - c.owner.cells.length,   // how many cells can split
-    threshold = c._mass - cellsLeft * minSize;                                  // size check for exploding cells
-
+    var minSize = this.gameServer.config.playerMinSize, // maximum size of small splits
+        cellsLeft = this.gameServer.config.playerMaxCells - c.owner.cells.length, // how many cells can split
+        threshold = c._mass - cellsLeft * minSize; // size check for exploding cells
     // Diverse explosion(s)
     var big = []; // amount of big splits
     if (cellsLeft <= 0) return; // cannot split
-    else if (cellsLeft == 1) big = [c._mass/2];
-    else if (cellsLeft == 2) big = [c._mass/4,c._mass/4];
-    else if (cellsLeft == 3) big = [c._mass/4,c._mass/4,c._mass/7];
-    else if (cellsLeft == 4) big = [c._mass/5,c._mass/7,c._mass/8,c._mass/10];
+    else if (cellsLeft == 1) big = [c._mass / 2];
+    else if (cellsLeft == 2) big = [c._mass / 4, c._mass / 4];
+    else if (cellsLeft == 3) big = [c._mass / 4, c._mass / 4, c._mass / 7];
+    else if (cellsLeft == 4) big = [c._mass / 5, c._mass / 7, c._mass / 8, c._mass / 10];
     // Monotone explosion(s)
     else if (c._size > 216) {
         // virus explosion multipliers
@@ -64,13 +60,10 @@ Virus.prototype.onEaten = function (c) {
         this.gameServer.splitPlayerCell(c.owner, c, angle, minSize);
     }
 };
-
-Virus.prototype.onAdd = function (gameServer) {
+Virus.prototype.onAdd = function(gameServer) {
     gameServer.nodesVirus.push(this);
 };
-
-Virus.prototype.onRemove = function (gameServer) {
+Virus.prototype.onRemove = function(gameServer) {
     var index = gameServer.nodesVirus.indexOf(this);
-    if (index != -1) 
-        gameServer.nodesVirus.splice(index, 1);
+    if (index != -1) gameServer.nodesVirus.splice(index, 1);
 };

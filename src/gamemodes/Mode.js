ce8220d3a8fc51f33ed0065e5a18bd22e48447f7
Agarian <Agarian@users.noreply.github.com>
2017-04-09 20:30:40 -0400

    "Beautify" Mode

--- src/gamemodes/Mode.js
+++ src/gamemodes/Mode.js
@@ -6,43 +6,33 @@ function Mode() {
     this.haveTeams = false; // True = gamemode uses teams, false = gamemode doesnt use teams
     this.specByLeaderboard = false; // false = spectate from player list instead of leaderboard
 }
-
 module.exports = Mode;
-
 // Override these
-
-Mode.prototype.onServerInit = function (gameServer) {
+Mode.prototype.onServerInit = function(gameServer) {
     // Called when the server starts
     gameServer.run = true;
 };
-
-Mode.prototype.onTick = function (gameServer) {
+Mode.prototype.onTick = function(gameServer) {
     // Called on every game tick 
 };
-
-Mode.prototype.onPlayerInit = function (player) {
+Mode.prototype.onPlayerInit = function(player) {
     // Called after a player object is constructed
 };
-
-Mode.prototype.onPlayerSpawn = function (gameServer, player) {
+Mode.prototype.onPlayerSpawn = function(gameServer, player) {
     // Called when a player is spawned
     player.setColor(gameServer.getRandomColor()); // Random color
     gameServer.spawnPlayer(player, gameServer.randomPos());
 };
-
-Mode.prototype.onCellAdd = function (cell) {
+Mode.prototype.onCellAdd = function(cell) {
     // Called when a player cell is added
 };
-
-Mode.prototype.onCellRemove = function (cell) {
+Mode.prototype.onCellRemove = function(cell) {
     // Called when a player cell is removed
 };
-
-Mode.prototype.onCellMove = function (cell, gameServer) {
+Mode.prototype.onCellMove = function(cell, gameServer) {
     // Called when a player cell is moved
 };
-
-Mode.prototype.updateLB = function (gameServer) {
+Mode.prototype.updateLB = function(gameServer) {
     // Called when the leaderboard update function is called
     gameServer.leaderboardType = this.packetLB;
 };

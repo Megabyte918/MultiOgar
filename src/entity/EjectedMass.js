61e68ece5e1631511d48629be2884118e22dbb8b
Agarian <Agarian@users.noreply.github.com>
2017-04-09 20:23:50 -0400

    "Beautify" EjectedMass

--- src/entity/EjectedMass.js
+++ src/entity/EjectedMass.js
@@ -1,22 +1,18 @@
-﻿var Cell = require('./Cell');
+﻿
+var Cell = require('./Cell');
 
 function EjectedMass() {
     Cell.apply(this, Array.prototype.slice.call(arguments));
-    
     this.cellType = 3;
 }
-
 module.exports = EjectedMass;
 EjectedMass.prototype = new Cell();
-
 // Main Functions
-
-EjectedMass.prototype.onAdd = function (gameServer) {
+EjectedMass.prototype.onAdd = function(gameServer) {
     // Add to list of ejected mass
     gameServer.nodesEjected.push(this);
 };
-
-EjectedMass.prototype.onRemove = function (gameServer) {
+EjectedMass.prototype.onRemove = function(gameServer) {
     // Remove from list of ejected mass
     var index = gameServer.nodesEjected.indexOf(this);
     if (index != -1) {

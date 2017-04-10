b920d8eaa1c6f5a5fec2600c897e1d2d0a0e2d9a
Agarian <Agarian@users.noreply.github.com>
2017-04-09 20:44:58 -0400

    "Beautify" BinaryReader

--- src/packet/BinaryReader.js
+++ src/packet/BinaryReader.js
@@ -6,37 +6,30 @@
  * Copyright (c) 2016 Barbosik https://github.com/Barbosik
  * License: Apache License, Version 2.0
  */
-
 function BinaryReader(buffer) {
     this._offset = 0;
     this._buffer = parseInt(process.version[1]) < 6 ? new Buffer(buffer) : Buffer.from(buffer);
 }
-
 module.exports = BinaryReader;
-
-BinaryReader.prototype.readInt16 = function () {
+BinaryReader.prototype.readInt16 = function() {
     var value = this._buffer.readInt16LE(this._offset);
     this._offset += 2;
     return value;
 };
-
-BinaryReader.prototype.readInt32 = function () {
+BinaryReader.prototype.readInt32 = function() {
     var value = this._buffer.readInt32LE(this._offset);
     this._offset += 4;
     return value;
 };
-
-BinaryReader.prototype.readDouble = function () {
+BinaryReader.prototype.readDouble = function() {
     var value = this._buffer.readDoubleLE(this._offset);
     this._offset += 8;
     return value;
 };
-
-BinaryReader.prototype.skipBytes = function (length) {
+BinaryReader.prototype.skipBytes = function(length) {
     this._offset += length;
 };
-
-BinaryReader.prototype.readStringZeroUtf8 = function () {
+BinaryReader.prototype.readStringZeroUtf8 = function() {
     var length = 0;
     var endLength = 0;
     for (var i = this._offset; i < this._buffer.length; i++) {
@@ -50,8 +43,7 @@ BinaryReader.prototype.readStringZeroUtf8 = function () {
     this._offset += endLength;
     return value;
 };
-
-BinaryReader.prototype.readStringZeroUnicode = function () {
+BinaryReader.prototype.readStringZeroUnicode = function() {
     var length = 0;
     var endLength = ((this._buffer.length - this._offset) & 1) != 0 ? 1 : 0;
     for (var i = this._offset; i + 1 < this._buffer.length; i += 2) {

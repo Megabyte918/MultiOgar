a123c3ae2da026e4769d810f8ce77e62afa56d91
Tyler3D <Tylermanrique@gmail.com>
2017-06-12 15:13:24 -0400

    Create Arrow.js

---
+++ src/packet/Arrow.js
@@ -0,0 +1,25 @@
+var BinaryWriter = require('./BinaryWriter');
+
+function Arrow (friends, set) {
+	this.friends = friends;
+}
+
+module.exports = Arrow;
+
+Arrow.prototype.build = function(protocol) {
+	var writer = new BinaryWriter();
+		writer = this.build160(writer);
+	return writer.toBuffer();
+};
+
+Arrow.prototype.build160 = function(writer, playerTracker) {
+	writer.writeUInt8(0xA0);
+	for (var i in this.friends) {
+	var friend = this.friends[i];
+	writer.writeUInt16(friend.cells[0].position.x >> 0);
+	writer.writeUInt16(friend.cells[0].position.y >> 0);
+	writer.writeStringZeroUtf8(friend._name);
+	console.log(friend.cells[0].position.x + " " + friend.cells[0].position.y + " " + friend._name)
+};
+	return writer;
+};
